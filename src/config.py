50a97eb693517ebc616beea71832cd4f17ae740a
sharkmoos <muddy117@gmail.com>
2022-05-12 09:39:08 +0100

    Update config.py

--- src/config.py
+++ src/config.py
@@ -4,3 +4,8 @@ host = environ["CTFD_HOST"]
 api_token = environ["CTFD_API_KEY"]
 sleep_time = 30 # seconds
 webhook_url = environ["DISCORD_WEBHOOK_URL"]
+
+
+print(host)
+print(api_token)
+print(webhook_url)
